3a1b15f34b83256d7bfc33b07f030254861379ae
Yotam Barnoy <yotambarnoy@gmail.com>
2020-01-02 22:16:05 -0500

    Move to scripts

---
+++ scripts/battlefront2.js
@@ -0,0 +1,38 @@
+Game.KillMutex = [
+   "PlayBF",
+];
+Game.FileSymlinkExclusions = [
+       "steam_api.dll",
+    "steam_appid.txt",
+];
+Game.NeedsSteamEmulation = false;
+Game.UseGoldberg = true;
+Game.HandlerInterval = 100;
+Game.SymlinkExe = false;
+Game.SymlinkGame = true;
+Game.SymlinkFolders = true;
+Game.SupportsKeyboard = true;
+Game.ExecutableName = "battlefrontii.exe";
+Game.SteamID = "6060";
+Game.GUID = "Battlefront 2 Classic";
+Game.GameName = "Star Wars Battlefront 2 (Classic, 2005)";
+Game.MaxPlayers = 64;
+Game.MaxPlayersOneMonitor = 16;
+Game.BinariesFolder = "";
+Game.LauncherTitle = "";
+Game.StartArguments = "/win";
+Game.HideTaskbar = false;
+Game.Hook.ForceFocus = true;
+Game.Hook.ForceFocusWindowName = "Star Wars Battlefront II";
+Game.Hook.DInputForceDisable = false;
+Game.Hook.DInputEnabled = true;
+Game.Hook.XInputEnabled = true;
+Game.Hook.XInputReroute = false;
+Game.Hook.CustomDllEnabled= false;
+Game.HookFocus= true;
+Game.HookInit= true;
+Game.HookInitDelay = false;
+//Game.KillMutexDelay = 10; 
+Game.UseDevReorder = true;
+Game.SetWindowHook = true;
+Game.PauseBetweenStarts = 25;
